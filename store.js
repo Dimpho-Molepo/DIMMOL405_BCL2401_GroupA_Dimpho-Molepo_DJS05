07d5b1416db7c7b26eda21ff07a367d2f338791f
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-24 12:52:06 +0200

    Added an update function to update the the store

--- store.js
+++ store.js
@@ -5,3 +5,16 @@ const Initial = {
 let states = [Initial];
 const notifiers = [];
 
+export const update = (action) => {
+  if (typeof action !== 'function'){
+    throw new Error('action is required to be a function')
+  }
+
+  const prev = Object.freeze({...states[0]})
+  const next = Object.freeze({...action(prev)})
+
+  const handler = (notify) => notify(prev, next)
+  notifiers.forEach(handler)
+  states.unshift(next)
+}
+
