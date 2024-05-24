4100d836e714acbd7b1e5730308178b10464a2c5
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-24 15:28:28 +0200

    Added some comments

--- store.js
+++ store.js
@@ -1,8 +1,22 @@
 import { Initial } from "./initialState.js";
 
+/** The states array holds the current and previous states of the store.
+ * 
+ * @type {Array<Object>}
+ */
 let states = [Initial];
+
+/** The notifiers array holds the functions that are subscribed to the store.
+ * 
+ * @type {Array<Function>}
+ */
 const notifiers = [];
 
+/** Updates the state of the store with a given action.
+ * 
+ * @param {Function} action - A function that takes the current state as an argument and returns the next state.
+ * @returns {void} This function doesn't return anything
+ */
 export const update = (action) => {
   if (typeof action !== 'function'){
     throw new Error('action is required to be a function')
@@ -16,6 +30,11 @@ export const update = (action) => {
   states.unshift(next)
 }
 
+/** Subscribes a function to the store, which will be called whenever the state of the store is updated.
+ * 
+ * @param {Function} notify - A function that takes the previous and next states as arguments.
+ * @returns {Function} unsubscribe - A function that unsubscribes the given function from the store.
+ */
 export const subscribe = (notify) => {
   notifiers.push(notify)
 
@@ -27,8 +46,18 @@ export const subscribe = (notify) => {
   return unsubscribe
 }
 
+/** Retrieves a value from the current state of the store.
+ * 
+ * @param {Object} state - The current state of the store.
+ * @param {string} key - The key of the value to retrieve.
+ * @returns {*} The value associated with the given key in the current state.
+*/
 export const get = (state, key) => {
     return state[key]
 }
 
+/** Exports the states array.
+ * 
+ * @type {Array<Object>}
+*/
 export { states };
